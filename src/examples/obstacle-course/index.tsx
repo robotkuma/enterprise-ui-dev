23492ec8169a0c563f2fb9e7ffd185cf1bd5a7ea
Kuma Bayne <kuma@robotcache.com>
2024-01-25 15:56:27 -0800

    Fix failing test

--- src/examples/obstacle-course/index.tsx
+++ src/examples/obstacle-course/index.tsx
@@ -52,7 +52,6 @@ const ObstacleCourse = () => {
   return (
     <div className="grid grid-cols-2 gap-4">
       <div className="flex items-center gap-2 border-2 border-purple-300 p-2">
-        <input />
         <label htmlFor="deep-thought">Deep Thought</label>
         <input
           placeholder="Some text…"
