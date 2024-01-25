c4f258a26e4d75b9f58e2074be8301ab5f7f4571
Kuma Bayne <kuma@robotcache.com>
2024-01-25 15:53:59 -0800

    Add tests

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,5 +1,18 @@
-import { render, screen } from 'test/utilities';
-import PackingList from '.';
+import { render as _render, screen, waitFor } from 'test/utilities';
+import { PackingList } from '.';
+import { createStore } from './store';
+import { Provider } from 'react-redux';
+import React, { Component, PropsWithChildren } from 'react';
+
+const render: typeof _render = (Component, options) => {
+  const store = createStore();
+
+  const Wrapper = ({ children }: PropsWithChildren) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return _render(Component, { ...options, wrapper: Wrapper });
+};
 
 it('renders the Packing List application', () => {
   render(<PackingList />);
@@ -10,19 +23,54 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' });
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+  expect(newItemInput).toHaveValue('');
+  expect(addNewItemButton).toBeDisabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' });
+
+  await user.type(newItemInput, 'MacBook Pro');
+
+  expect(addNewItemButton).toBeEnabled();
+});
+
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' });
+
+  await user.type(newItemInput, 'MacBook Pro');
+  await user.click(addNewItemButton);
+
+  expect(screen.getByLabelText('MacBook Pro')).not.toBeChecked();
+});
+
+it('Removes an item', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' });
+
+  await user.type(newItemInput, 'MacBook Pro');
+  await user.click(addNewItemButton);
+
+  screen.debug();
+
+  const removeItem = screen.getByLabelText(/remove/i);
+
+  await user.click(removeItem);
+
+  waitFor(() => expect(removeItem).not.toBeInTheDocument());
+});
